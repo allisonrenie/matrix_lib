bd875b7d877fea09552cfdd1c14c7329c922230d
allisonrenie <allisonrenie@gmail.com>
2023-06-14 00:52:17 -0700

    small start and reconfig of files

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+struct Mat3
+{
+  // row 0
+  x0: f32,
+  y0: f32,
+  z0: f32,
+
+  // row 1
+  x1: f32,
+  y1: f32,
+  z1: f32,
+
+  // row 2
+  x2: f32,
+  y2: f32,
+  z2: f32,
+}
+
+struct Vec3
+{
+  x: f32,
+  y: f32,
+  z: f32,
+}
+
+pub fn rotation()
+{
+
+}
+
+pub fn transform()
+{
+  
+}
